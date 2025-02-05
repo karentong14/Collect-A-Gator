8b369275bac2b7d767a728456dc1cffe6a7b813c
ealfla8 <68116219+ealfla8@users.noreply.github.com>
2025-02-04 19:41:07 -0500

    intro web app

---
+++ windows/SeniorProjectReactNativeTest/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::SeniorProjectReactNativeTest::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::SeniorProjectReactNativeTest::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
