ee41ce1607d9c46cf8098ee33bf04f86d502fa05
karentong14 <karentong@ufl.edu>
2025-02-09 16:12:43 -0500

    still trying to add new page

--- collect-a-gator/app/page.tsx
+++ collect-a-gator/app/page.tsx
@@ -96,6 +96,9 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <>
+        <Link href="/map">map</Link>
+        </>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="/map"
