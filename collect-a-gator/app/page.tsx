0fb6e2ca7ad149269c3aa01b93132a4929c1fd73
karentong14 <karentong@ufl.edu>
2025-02-09 16:23:07 -0500

    map page made!

--- collect-a-gator/app/page.tsx
+++ collect-a-gator/app/page.tsx
@@ -96,9 +96,6 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-        <>
-        <Link href="/map">map</Link>
-        </>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="/map"
