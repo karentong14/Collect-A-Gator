9def542ffdcf97367cd24c3a15e729ec8edef0f6
karentong14 <karentong@ufl.edu>
2025-02-09 22:59:39 -0500

    added map!

--- collect-a-gator/app/map/page.tsx
+++ collect-a-gator/app/map/page.tsx
@@ -1,20 +1,20 @@
-import Link from 'next/link';
-// import {createRoot} from "react-dom/client"; //idk what this does
-import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
+"use client";
+import React from 'react'
+import {AdvancedMarker, APIProvider, Map} from '@vis.gl/react-google-maps';
 
-const App = () => (
- <APIProvider apiKey={'AIzaSyDzlnZvBWEtR8J_jA-VMy32fsa5M9YREls'} onLoad={() => console.log('Maps API has loaded.')}>
-   <Map
-      defaultZoom={13}
-      defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
-      onCameraChanged={ (ev: MapCameraChangedEvent) =>
-        console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-      }>
-   </Map>
- </APIProvider>
-);
 
-const root = createRoot(document.getElementById('app'));
-root.render(<App />);
+function App() {
+    const position = { lat: 29.6520, lng: -82.3250 };
+
+    return (
+        <APIProvider apiKey={"AIzaSyC-Pip5d3p8_6swFtL_hRosMm2VTpraip4"}>
+            <div style={{ width: "100vw", height: "100vh" }}>
+                <Map defaultCenter={position} defaultZoom={10} mapId="5174ed5358f23a3c">
+                    <AdvancedMarker position={position} />
+                </Map>
+            </div>
+        </APIProvider>
+    );
+}
 
 export default App;
