02018ec26b6526648d45b903242bde29cc2944d9
gabeisenberg <gwberg20@gmail.com>
2025-02-28 16:09:27 -0500

    temporary post on journal entry

--- collect-a-gator/app/journal/newEntry/page.tsx
+++ collect-a-gator/app/journal/newEntry/page.tsx
@@ -13,18 +13,64 @@ import Textarea from '@mui/joy/Textarea';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { useEffect, useRef } from 'react';
+import { JournalEntry } from '@/components/models/models';
 
 export default function EntryPage({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-    /* UNCOMMENT BELOW TO USE DATE SPECIFICS FOR LATER */
-    //const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17'));
+    const [date, setDate] = React.useState<Dayjs>(dayjs());
+    const [title, setTitle] = React.useState<null | string>();
+    const [content, setContent] = React.useState<null | string>();
+    const [trigger, setTrigger] = React.useState<boolean>(false);
+
+    useEffect(() => {
+        setDate(dayjs()); 
+    }, []);
+
+    const handleDateChange = (newDate: Dayjs | null) => {
+        if (newDate) setDate(newDate);
+    };
+
+    const submitEntry = () => {
+        if (title && content) {
+            setTrigger(!trigger);
+        }
+    };
+
+    const isFirstRender = useRef(true);
+
+    useEffect(() => {
+        if (isFirstRender.current) {
+            isFirstRender.current = false;
+        }
+        else {
+            const fetchData = async () => {
+                const response = await fetch("http://localhost:5050/api", {
+                  method: "POST",
+                  body: JSON.stringify({
+                      title: title,
+                      date: date,
+                      content: content,
+                      id: 0
+                  }),
+                  headers: {
+                      "Content-type": "application/json; charset=UTF-8"
+                  }
+                })
+                .then(response => response.json())
+                .catch(error => console.error(error));
+              };
+              fetchData();
+        }
+      }, [trigger]);
+
     return (
         <Card sx={{
             padding: '20px',
-            maxWidth: '800px',
+            maxWidth: 'lg',
             margin: 'auto'
         }}>
             <Grid container spacing={2} sx={{
@@ -39,16 +85,19 @@ export default function EntryPage({
                 </Grid>
                 <Grid item xs={12} sm={10} md={8}>
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker defaultValue={dayjs('2025-02-01')} />
+                        <DatePicker defaultValue={date} format="YYYY-MM-DD" onChange={handleDateChange}/>
                     </LocalizationProvider>
                 </Grid>
                 <Grid item xs={12} sm={10} md={8}>
-                    <Textarea placeholder="Type anything…"></Textarea>
+                    <Textarea placeholder="Enter a title name..." onChange={(e) => setTitle(e.target.value)}></Textarea>
+                </Grid>
+                <Grid item xs={12} sm={10} md={8}>
+                    <Textarea placeholder="Type anything…" onChange={(e) => setContent(e.target.value)}></Textarea>
                 </Grid>
                 <Grid item xs={12} sm={10} md={8}>
                     <Grid container spacing={2}>
                         <Grid item>
-                            <Button variant="contained">Submit</Button>
+                            <Button variant="contained" onClick={submitEntry}>Submit</Button>
                         </Grid>
                         <Grid item>
                             <Button variant="contained" color="error">Remove</Button>
